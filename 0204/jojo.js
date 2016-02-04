f5e45562340cf3ae9a5996e26129e49d38bca2ba
Misa Pham <mpham3@lion.lmu.edu>
2016-02-04 13:27:15 -0800

    Fixed jojo's image loading delay

--- 0204/jojo.js
+++ 0204/jojo.js
@@ -1,7 +1,7 @@
 (function () {
 	
 	window.SpriteLibrary = window.SpriteLibrary || {}; 
-	var ARM_WIDTH = 30;
+	var ARM_WIDTH = 35;
 	var ARM_HEIGHT = 160;
 
 	var jojoImage = new Image();
@@ -22,6 +22,8 @@
 	var BODY_HEIGHT = dressImage.height;
 
 	var drawArm = function(renderingContext, armAngle, armXOffset, armYOffset) {
+		
+
 		renderingContext.save();
 		renderingContext.translate(-BODY_WIDTH / 2 + armXOffset, -BODY_HEIGHT / 2 + armYOffset);
 		renderingContext.rotate(armAngle);
@@ -30,15 +32,20 @@
 		
 		renderingContext.beginPath();
 		var fistRadius = ARM_WIDTH / 2;
-		renderingContext.arc(0, ARM_HEIGHT + fistRadius / 2, fistRadius, 0 , 2 * Math.PI, true);
+		var fistPosition = ARM_HEIGHT + fistRadius - 5;
+		renderingContext.arc(0, fistPosition, fistRadius, 0 , 2 * Math.PI, true);
 		renderingContext.fill();
 		renderingContext.restore();
 	}
 
 	SpriteLibrary.jojo = function (jojoSpecification) {
+		if (!jojoLoaded || !dressLoaded) {
+			return;
+		}
+
 		var renderingContext = jojoSpecification.renderingContext;
-		var leftArmAngle = jojoSpecification.leftArmAngle || (Math.PI / 4);
-		var rightArmAngle = jojoSpecification.rightArmAngle || (-Math.PI / 4);
+		var leftArmAngle = jojoSpecification.leftArmAngle || (Math.PI / 6);
+		var rightArmAngle = jojoSpecification.rightArmAngle || (-Math.PI / 6);
 
 		var dressOffset = 35;
 		var armYOffset = 150;
@@ -47,38 +54,19 @@
 		
 		drawArm(renderingContext, rightArmAngle, rightArmXOffset, armYOffset);
 		drawArm(renderingContext, leftArmAngle, -leftArmXOffset, armYOffset);
-		
+
 		renderingContext.save();
 		if (jojoLoaded) {
 			renderingContext.scale(.9, .9);
 			renderingContext.drawImage(jojoImage, -jojoImage.width, -jojoImage.height);
 		}
 		renderingContext.restore();
-
+		
 		renderingContext.save();
-		if(dressLoaded) {
+		if(dressLoaded && jojoLoaded) {
 			renderingContext.drawImage(dressImage, -BODY_WIDTH - dressOffset, -jojoImage.height/2 + 60);
 		}
 		renderingContext.restore();
-
-		/*renderingContext.save();
-		renderingContext.translate(-BODY_WIDTH / 2 + rightArmXOffset, -BODY_HEIGHT / 2 + armYOffset);
-		renderingContext.rotate(rightArmAngle);
-		renderingContext.fillStyle = "#DABBA5";
-		renderingContext.fillRect(-ARM_WIDTH / 2, 0, ARM_WIDTH, ARM_HEIGHT);
-		renderingContext.restore();
-
-		//renderingContext.translate(BODY_WIDTH / 2 + armXOffset, BODY_HEIGHT / 2 - armYOffset);
-		//renderingContext.rotate(armAngle);
-		//renderingContext.fillRect(-ARM_WIDTH / 2, 0, ARM_WIDTH, ARM_HEIGHT);
-
-		renderingContext.save();
-		renderingContext.translate(-BODY_WIDTH / 2 - leftArmXOffset, -BODY_HEIGHT / 2 + armYOffset);
-		renderingContext.rotate(leftArmAngle);
-		renderingContext.fillStyle = "#DABBA5";
-		renderingContext.fillRect(-ARM_WIDTH / 2, 0, ARM_WIDTH, ARM_HEIGHT);
-		renderingContext.restore();*/
-
 	}
 
 }());
