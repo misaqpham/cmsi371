ea49171df778eb1f084319083789a29d40937687
Misa Pham <mpham3@lion.lmu.edu>
2016-03-28 01:00:08 -0700

    rotation matrix + webgl conversion

--- pipeline/matrix/matrix.js
+++ pipeline/matrix/matrix.js
@@ -1,16 +1,3 @@
-/*
- * This JavaScript file defines a Vector object and associated functions.
- * The object itself is returned as the result of a function, allowing us
- * to encapsulate its code and module variables.
- *
- * This module's approach is non-destructive: methods always return new
- * Vector objects, and never modify the operands.  This is a design choice.
- *
- * This module is designed for vectors of any number of dimensions.  The
- * implementations are generalized but not optimal for certain sizes of
- * vectors.  Specific Vector2D and Vector3D implementations can be much
- * more compact, while sacrificing generality.
- */
 var Matrix = (function () {
     // Define the constructor.
     var matrix = function () {
@@ -34,7 +21,11 @@ var Matrix = (function () {
         return this.elements.length;
     };
 
-    // Scalar multiplication and division.
+    matrix.prototype.elements = function () {
+        return this.elements;
+    };
+
+
     matrix.prototype.multiply = function (matrix2) {
         var result = new Matrix(),
             column,
@@ -100,106 +91,43 @@ var Matrix = (function () {
         var zs = z * s;
 
         // GL expects its matrices in column major order.
-        return [
-            (x2 * oneMinusC) + c,
-            (xy * oneMinusC) + zs,
-            (xz * oneMinusC) - ys,
-            0.0,
+        var rotation = new Matrix (
+            (x2 * oneMinusC) + c,   (xy * oneMinusC) + zs,  (xz * oneMinusC) - ys,  0.0,
+            (xy * oneMinusC) - zs,  (y2 * oneMinusC) + c,   (yz * oneMinusC) + xs,  0.0,
+            (xz * oneMinusC) + ys,  (yz * oneMinusC) - xs,  (z2 * oneMinusC) + c,   0.0,
+            0.0,                    0.0,                    0.0,                    1.0
+        );
 
-            (xy * oneMinusC) - zs,
-            (y2 * oneMinusC) + c,
-            (yz * oneMinusC) + xs,
-            0.0,
-
-            (xz * oneMinusC) + ys,
-            (yz * oneMinusC) - xs,
-            (z2 * oneMinusC) + c,
-            0.0,
-
-            0.0,
-            0.0,
-            0.0,
-            1.0
-        ];
+        return rotation;
     };
 
     matrix.getOrthogonalMatrix = function (r, l, t, b, f, n) {
-        var orthogonal = new Matrix( 2/(r-l),   0,          0,          -((r+l)/(r-l)),
+        var orthogonal = new Matrix(  2/(r-l),  0,          0,          -((r+l)/(r-l)),
                                       0,        2/(t-b),    0,          -((t+b)/(t-b)),
                                       0,        0,          -2/(f-n),   -((f+n)/(f-n)),
-                                      0,        0,          0,          1 );
-
-
-
+                                      0,        0,          0,          1               
+                                    );
         return orthogonal;
     };
 
-    matrix.getProjectionMatrix = function (r, l, t, b, f, n) {
+    matrix.getPerspectiveProjectionMatrix = function (r, l, t, b, f, n) {
         var projection = new Matrix( (2*n)/(r-l),   0,              (r+l)/(r-l),        0,
                                       0,            (2*n)/(t-b),    (t+b)/(t-b),        0,
                                       0,            0,              -((f+n)/(f-n)),     (-2*n*f)/(f-n),
-                                      0,            0,              -1,                 0 );
-
-
-
+                                      0,            0,              -1,                 0               
+                                    );
         return projection;
     };
- 
-    // Dot product.
-    vector.prototype.dot = function (v) {
-        var result = 0,
-            i,
-            max;
 
-        // Dimensionality check.
-        checkDimensions(this, v);
+    matrix.convertForWebGL = function () {
+        var columnsForWebGL = [],
+            i;
 
-        for (i = 0, max = this.dimensions(); i < max; i += 1) {
-            result += this.elements[i] * v.elements[i];
+        for (i = 0; i<4; i++) {
+            columnsForWebGL.push(this.elements(i), this.elements(i + 4), this.elements(i + 8), this.elements(i + 12))
         }
 
-        return result;
-    };
+        return columnsForWebGL
+    }
 
-    // Cross product.
-    vector.prototype.cross = function (v) {
-        // This method is for 3D vectors only.
-        if (this.dimensions() !== 3 || v.dimensions() !== 3) {
-            throw "Cross product is for 3D vectors only.";
-        }
-
-        // With 3D vectors, we can just return the result directly.
-        return new Vector(
-            (this.y() * v.z()) - (this.z() * v.y()),
-            (this.z() * v.x()) - (this.x() * v.z()),
-            (this.x() * v.y()) - (this.y() * v.x())
-        );
-    };
-
-    // Magnitude and unit vector.
-    vector.prototype.magnitude = function () {
-        // Make use of the dot product.
-        return Math.sqrt(this.dot(this));
-    };
-
-    vector.prototype.unit = function () {
-        // At this point, we can leverage our more "primitive" methods.
-        return this.divide(this.magnitude());
-    };
-
-    // Projection.
-    vector.prototype.projection = function (v) {
-        var unitv;
-
-        // Dimensionality check.
-        checkDimensions(this, v);
-
-        // Plug and chug :)
-        // The projection of u onto v is u dot the unit vector of v
-        // times the unit vector of v.
-        unitv = v.unit();
-        return unitv.multiply(this.dot(unitv));
-    };
-
-    return vector;
 })();
